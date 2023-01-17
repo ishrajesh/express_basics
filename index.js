0da5696a7ad52a37ee00f2c85dacaeeba26ff97f
ishrajesh <ishrajeshshelley@gmail.com>
2023-01-17 14:49:05 +0530

    Adding route to add all the members

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const members = require('./Members');
+
 
 // // instead of setting routes for all paths, 
 // // we can set the folder as static folder
 // app.get('/',(req,res) => { 
 //     res.sendFile(path.join(__dirname,'public','index.html'));
 //  })
+
+
+// This route gets all members
+app.get('/api/members', (req,res)=> res.json(members))
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
